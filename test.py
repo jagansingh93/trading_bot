fb00f462f410ebcccd54ca2504291464219917e4
Jagandeep Singh <jagansingh93@gmail.com>
2021-02-26 15:56:51 -0500

    test

--- test.py
+++ test.py
@@ -3,12 +3,23 @@ import requests
 import json
 from config import *
 
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
-HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 
 def get_account():
     r = requests.get(ACCOUNT_URL, headers=HEADERS)
     return json.loads(r.content)
 
-print(get_account())
+def create_order(symbol, qty, side, type, time_in_force):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side":side,
+        "type":type,
+        "time_in_force":time_in_force
+    }
+    r = requests.post(ORDERS_URL, json=data, headers = HEADERS)
+    return json.loads(r.content)
+
