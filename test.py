d8ffe934ec166bec408b07e0062e96567ae0d942
Jagandeep Singh <jagansingh93@gmail.com>
2021-02-26 15:41:51 -0500

    Testing Alpaca with paper trading

--- test.py
+++ test.py
@@ -5,11 +5,10 @@ from config import *
 
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
 def get_account():
-    r = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
     return json.loads(r.content)
-
-
 
 print(get_account())
